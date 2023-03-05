ef3b0e66f585ec039f352fa5586fedb82cff2718
ManishinGithub1 <116179123+ManishinGithub1@users.noreply.github.com>
2023-03-05 19:45:57 +0530

    Create Interest.py

---
+++ Interest.py
@@ -0,0 +1,6 @@
+#program for simple_interest
+principal=float(input("Enter the pricipal amount:"))
+time=float(input("Enter the time:"))
+rate=float(input("Enter the rate:"))
+Simple_Interest =(principal*time*rate)/100
+print("Simple Interest is:",Simple_Interest)
